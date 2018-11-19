b34317f30e9e89cd4725fbd0959f7ebc57b2daf6
Lucifer <nileshsolanki456@gmail.com>
2018-11-19 12:37:36 +0530

    Updates

---
+++ precache-manifest.a60789e118f831d9a1f7384661dcc82a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "30f3c523e075970b27d5",
+    "url": "/weather-app/static/js/main.30f3c523.chunk.js"
+  },
+  {
+    "revision": "8c0daa4f81e3a3d060b8",
+    "url": "/weather-app/static/js/1.8c0daa4f.chunk.js"
+  },
+  {
+    "revision": "8c0daa4f81e3a3d060b8",
+    "url": "/weather-app/static/css/1.c18c2cc0.chunk.css"
+  },
+  {
+    "revision": "1c8c612ccf7c8f649d495e600113730d",
+    "url": "/weather-app/index.html"
+  }
+];
